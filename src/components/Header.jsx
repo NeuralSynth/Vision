8d25487308a3ba61dde0a96ef8725fc7be557347
Tushar Shaw <128092800+STusharADS@users.noreply.github.com>
2025-02-16 22:05:16 +0530

    commit

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,6 +2,8 @@ function Header(){
     return(
         <header>
             <h1>VisionOS</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit, totam quod. Autem nam animi error ut, nulla iste consectetur laudantium incidunt dolor, corrupti distinctio, aliquid vero est neque quod minus.</p>
+        
         </header>
     )
 }
